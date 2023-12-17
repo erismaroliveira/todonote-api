5cb42599e485bc04bebb8abc8458e76d23af9037
Erismar Oliveira <erismarpro@hotmail.com>
2023-12-17 17:37:59 -0300

    configure project

---
+++ src/main/java/com/erismaroliveira/todonote/TodonoteApplication.java
@@ -0,0 +1,13 @@
+package com.erismaroliveira.todonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodonoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodonoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erismaroliveira/todonote/TodonoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erismaroliveira.todonote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodonoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
