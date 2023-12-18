2d14e9815e9026958302e629557b54a935776bfe
Erismar Oliveira <erismarpro@hotmail.com>
2023-12-18 10:37:39 -0300

    create user and task repositories

---
+++ src/main/java/com/erismaroliveira/todonote/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.erismaroliveira.todonote.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.erismaroliveira.todonote.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+  
+  List<Task> findByUser_Id(Long id);
+
+  // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+  // List<Task> findByUserId(@Param("id") Long id);
+
+  // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+  // List<Task> findByUserId(@Param("id") Long id);
+
+}
---
+++ src/main/java/com/erismaroliveira/todonote/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.erismaroliveira.todonote.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.erismaroliveira.todonote.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+  User findByUsername(String username);
+  
+}
